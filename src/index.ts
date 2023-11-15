54cfb5f8294a57b4ba68b0ee06d2369f28ec8f7c
ramiro elisei <ramiroelisei@gmail.com>
2023-11-15 22:27:47 +0100

    Add ping pong

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import app from "./app.js";
 import morgan from "morgan";
+import app from "./app.js";
+import express from "express";
+import pingRouter from "./features/ping/router/pingRouter.js";
 
 app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/", pingRouter);
