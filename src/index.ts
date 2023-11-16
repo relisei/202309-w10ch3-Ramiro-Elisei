b7fb6cd8334d41cdc7248365febdbc87f58c4afc
ramiro elisei <ramiroelisei@gmail.com>
2023-11-16 20:25:15 +0100

    Add database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import "./server/index.js";
 import { startServer } from "./server/app.js";
+import connectToDataBase from "./database/index.js";
+import chalk from "chalk";
 
 const port = process.env.PORT ?? 4000;
+if (!process.env.MONGODB_URL) {
+  console.log(chalk.red("Missing MongoDB String"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDataBase(mongoUrl);
 
 startServer(+port);
