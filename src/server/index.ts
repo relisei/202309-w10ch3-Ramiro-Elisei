91f234a490f94293b2efaaeb1b98bdebb5b59172
ramiro elisei <ramiroelisei@gmail.com>
2023-11-16 23:40:55 +0100

    Add beersRouter to server index

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,10 @@ import morgan from "morgan";
 import express from "express";
 import { app } from "./app.js";
 import pingRouter from "../features/ping/router/pingRouter.js";
+import beersRouter from "../features/beers/router/beersRouter.js";
 
 app.use(express.json());
 app.use(morgan("dev"));
+
 app.use("/", pingRouter);
+app.use("/beers", beersRouter);
