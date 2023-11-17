baaaf9d6237af4e1046bc4dac66471712dbd05dd
ramiro elisei <ramiroelisei@gmail.com>
2023-11-17 14:01:31 +0100

    Add getBeersById to BeersController

--- src/features/beers/controller/BeersController.ts
+++ src/features/beers/controller/BeersController.ts
@@ -9,6 +9,18 @@ class BeerController {
 
     res.status(200).json({ beers });
   };
+
+  getBeersById = async (req: Request<{ beerId: string }>, res: Response) => {
+    try {
+      const { beerId } = req.params;
+
+      const currentBeer = await this.beersRepository.getBeersById(beerId);
+      res.status(200).json({ currentBeer });
+    } catch (error) {
+      console.log(error.message);
+      res.status(404).json({ error: "The searched id was not found" });
+    }
+  };
 }
 
 export default BeerController;
