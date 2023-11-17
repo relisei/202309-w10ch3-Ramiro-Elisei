3e57c5aa9a62d5549f5fa97adeaa6660da33e18c
ramiro elisei <ramiroelisei@gmail.com>
2023-11-17 14:02:46 +0100

    Add getBeersById to beersRouter

--- src/features/beers/repository/types.ts
+++ src/features/beers/repository/types.ts
@@ -2,4 +2,5 @@ import { type BeerStructure } from "../types";
 
 export interface BeersRepository {
   getBeers: () => Promise<BeerStructure[]>;
+  getBeersById(beerId: string): Promise<BeerStructure>;
 }
